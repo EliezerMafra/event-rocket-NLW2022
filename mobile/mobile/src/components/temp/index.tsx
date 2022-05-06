bf0c7ca5649099e6675a81da195c61a05688b11e
Eliezer Marques Mafra <32603174+EliezerMafra@users.noreply.github.com>
2022-05-06 11:50:36 -0300

    Finalização de aplicação react-native

---
+++ mobile/mobile/src/components/temp/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from "react-native";
+
+import { styles } from './styles';
+
+export function	Model(){
+	return(
+
+		<View style={styles.container}>
+
+		</View>
+
+	);
+}
--- mobile/mobile/src/components/temp/styles.ts
+++ mobile/mobile/src/components/temp/styles.ts
@@ -2,9 +2,5 @@ import { StyleSheet } from "react-native";
 import { theme } from "../../theme";
 
 export const styles = StyleSheet.create({
-	text: {
-		fontSize: 12,
-		color: theme.colors.text_secondary,
-		fontFamily: theme.fonts.medium,
-	}
+	container:{}
 });
