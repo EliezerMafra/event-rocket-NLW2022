eb5de7c3284cf7b081d7b654a746b5e99b14dcb2
Eliezer Marques Mafra <32603174+EliezerMafra@users.noreply.github.com>
2022-05-20 22:58:06 -0300

    Release Mobile

--- mobile/mobile/src/libs/api.ts
+++ mobile/mobile/src/libs/api.ts
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
+const api_URL = 'https://feedwid-production.up.railway.app/'
+const local_URL = 'http://192.168.0.157:3333'
+
 export const api = axios.create({
-	baseURL: 'http://192.168.0.157:3333'
+	baseURL: api_URL
 })
